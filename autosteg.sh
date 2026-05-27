09fec1c00a2df3715b50c91b01148ccc97f2dd97
Aidan <149415187+kennyfan382@users.noreply.github.com>
2026-05-27 23:27:39 +0100

    PNG + Enter text
    You can now type messages in the terminal if no text file is available in the directory the script is running in

--- autosteg.sh
+++ autosteg.sh
@@ -3,13 +3,13 @@
 #The user will be notified at the end whether the process worked or not
 
 AS(){
-    chmod +x AutomateSteganography.sh
     clear
     echo "Welcome to Aidan Hammonds automated steganography tool!"
-    echo "All possible .jpg files in directory":
-    sleep 2
+    echo "All possible .jpg & .png files in directory":
+    sleep 1
     ls *.jpg
-    sleep 2
+    ls *.png
+    sleep 1
     while true; do
     echo "Enter your image (remember to add file type on the end E.g MyDog.jpg): "
     read inputImage
@@ -18,56 +18,69 @@ AS(){
         echo "Received"
         sleep 1
         echo "Your image: $image"
-        sleep 2
+        sleep 1
         break
     else
         echo "File does not exist (if this persists, maybe try changing directories)"
-        sleep 3
+        sleep 1
     fi
     done
     clear
     echo "All possible .txt files in directory":
     ls *.txt
-    sleep 2
+    sleep 1
+    enteredMessage=0 #did the user enter their message (0 if used a text file, 1 otherwise)
     while true; do
-    echo "Enter your text file (has to end in .txt): "
+    echo "Enter your text file (has to end in .txt) OR type n (you can type your own message): "
     read inputTXT
     txt="$inputTXT"
+    if [ $inputTXT = "n" ]; then
+        echo "enter your message: "
+        sleep 1
+        read usrMsg
+        userMsg="$usrMsg"
+        enteredMessage=1
+        break
+    fi
     if [[ -f "$txt" ]]; then
         echo "Received"
         sleep 1
         echo "Your text file: $txt"
-        sleep 2
+        sleep 1
         break
     else
         echo "File does not exist (maybe try changing directories)"
-        sleep 2
+        sleep 1
     fi
     done
     clear
-    message=$(cat "$txt")
-    echo "Message: $message"
-    sleep 2
-    cat "$txt" >> "$image"
+    if [ $enteredMessage -eq 0 ]; then
+        message=$(cat "$txt")
+        echo "Message: $message"
+        sleep 1
+        cat "$txt" >> "$image"
+    else
+        echo "$usrMsg" >> "$image"
+    fi
     echo "Process complete!"
-    sleep 2
+    sleep 1
     echo "Wanna see? Y/n"
     read answer
     if [[ "$answer" == "Y" || "$answer" == "y" ]]; then
         echo -e "\n\n\n" #Ensures good seperation between command and the text within the image
         strings "$image" | tail -n 10
         echo -e "\n\n\nIf you want to see it again, type: strings "$image" | tail -n 10"
-        sleep 3
+        sleep 1
         echo -e "\n\nNOTE: you are viewing the last 10 lines of your text file, the whole message has been transferred to the image."
-        sleep 3
+        sleep 1
         echo -e "\nIf it was longer than 10 lines and you want to see the whole text in the image simply append the number in the command to something higher until you reach your desired output"
-        sleep 5
+        sleep 1
         echo -e "\nYou might also want to type 'cat $image' and then scroll to the bottom to find your full message"
-        sleep 8
+        sleep 1
     else
         echo -e "\nIf you want to see in the future, type: strings "$image" | tail -n 10"
         echo -e "\nIf your message is longer than 10 lines, please append the number at the end of the command to be higher or type cat $image then scroll to the bottom"
-        sleep 4
+        sleep 1
         echo "Okay, bye"
     fi
 }
